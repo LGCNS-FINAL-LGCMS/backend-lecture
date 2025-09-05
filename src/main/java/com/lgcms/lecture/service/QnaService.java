6fdf72f44bbea43100f4759e4765a45132d8ac43
하리보냠 <drppr99@gmail.com>
2025-09-05 13:53:40 +0900

    hotfix : 강의 질문 등록 수정

--- src/main/java/com/lgcms/lecture/service/QnaService.java
+++ src/main/java/com/lgcms/lecture/service/QnaService.java
@@ -53,13 +53,15 @@ public class QnaService {
                 .content(questionCreateRequest.getContent())
                 .lectureAnswers(new ArrayList<>())
                 .build();
+        lectureQuestionRepository.save(lectureQuestion);
+        System.out.println(lectureQuestion.getId());
+
         QnaCreated qnaCreated = QnaCreated.builder()
                 .lectureName(lecture.getTitle())
                 .memberId(lecture.getMemberId())
                 .qnaId(lectureQuestion.getId())
                 .build();
         qnaCreatedEvent.QnaCreated(qnaCreated);
-        lectureQuestionRepository.save(lectureQuestion);
         return lectureQuestion.getId();
     }
 
